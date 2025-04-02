eed15acae3ef254f7b56113945a2d57481892800
IgorBelevyat <beliaviat228@gmail.com>
2025-04-02 23:07:43 +0300

    creating a base structure for the shop

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
+
+const showOrders = (props) => {
+  return (<div>
+    {props.orders.map(el => (
+      <Order key={el.id} item = {el} />
+        ))
+     }
+  </div>)
+}
+
+const showNothing = () => {
+  return (<div className='empty'> 
+    <h2>Cart is empty</h2>
+  </div>)
+}
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false)
+
+  return (
+    <header>
+        <div>
+            <span className='logo'>Clothing store</span>
+            <ul className='nav'>
+                <li>About us</li>
+                <li>Contacts</li>
+                <li>My cabinet</li>
+            </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
+
+            {cartOpen && (
+              <div className='shop-cart'>
+                {props.orders.length > 0 ?
+                showOrders(props) : showNothing()  
+              }
+              </div>
+            )}
+        </div>
+        <div className='presentation'>
+
+        </div>
+    </header>
+  )
+}
