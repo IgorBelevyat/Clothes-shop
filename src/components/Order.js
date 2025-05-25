fa68e63b36a95427500e240f783489ca262a7537
IgorBelevyat <beliaviat228@gmail.com>
2025-05-25 23:48:24 +0300

    fixing displaing of image in order cart

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,7 @@ export class Order extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/" + this.props.item.img} alt='product'/>
+        <img src={this.props.item.image} alt='product'/>
         <h2>{this.props.item.title}</h2>
         <b>{this.props.item.price}$</b>
         <FaTrash className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)}/>
